4de81c38c2557e589509a8ea81b6438ed9a4bf2c
Svitlana Humen <138813162+SvitlanaHu@users.noreply.github.com>
2024-04-17 00:20:13 +0300

    add commit

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,69 +1,69 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
-import { getMovieCast } from "../API/tmdbApi";
-import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
-import { Loader } from "../Loader/Loader";
-import styles from "./Cast.module.css";
+// import { useEffect, useState } from "react";
+// import { useParams } from "react-router-dom";
+// import { getMovieCast } from "../API/tmdbApi";
+// import { ErrorMessage } from "../ErrorMessage/ErrorMessage";
+// import { Loader } from "../Loader/Loader";
+// import styles from "./Cast.module.css";
 
-const Cast = () => {
-  const defaultImg =
-    "https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700";
-  const { movieId } = useParams();
-  const [actors, setActors] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+// const Cast = () => {
+//   const defaultImg =
+//     "https://ireland.apollo.olxcdn.com/v1/files/0iq0gb9ppip8-UA/image;s=1000x700";
+//   const { movieId } = useParams();
+//   const [actors, setActors] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
 
-  useEffect(() => {
-    const controller = new AbortController();
-    const fetchData = async () => {
-      try {
-        setLoading(true);
-        setError(false);
-        const response = await getMovieCast({
-          movieId,
-          abortController: controller,
-        });
-        setActors(response);
-      } catch (error) {
-        if (error.code !== "ERR_CANCELED") {
-          setError(true);
-        }
-      } finally {
-        setLoading(false);
-      }
-    };
+//   useEffect(() => {
+//     const controller = new AbortController();
+//     const fetchData = async () => {
+//       try {
+//         setLoading(true);
+//         setError(false);
+//         const response = await getMovieCast({
+//           movieId,
+//           abortController: controller,
+//         });
+//         setActors(response);
+//       } catch (error) {
+//         if (error.code !== "ERR_CANCELED") {
+//           setError(true);
+//         }
+//       } finally {
+//         setLoading(false);
+//       }
+//     };
 
-    fetchData();
-    return () => {
-      controller.abort();
-    };
-  }, [movieId]);
+//     fetchData();
+//     return () => {
+//       controller.abort();
+//     };
+//   }, [movieId]);
 
-  return (
-    <div className={styles.container}>
-      {error && <ErrorMessage />}
-      {loading && <Loader />}
-      <ul className={styles.list}>
-        {actors.map((actor) => (
-          <li key={actor.id} className={styles.listItem}>
-            <div className={styles.box}>
-              <img
-                src={
-                  actor.profile_path
-                    ? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
-                    : defaultImg
-                }
-                alt="Actor photo"
-                width={100}
-                height={140}
-              />
-              <p className={styles.name}>{actor.name}</p>
-              <p className={styles.hero}>Character:{actor.character}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-export default Cast;
+//   return (
+//     <div className={styles.container}>
+//       {error && <ErrorMessage />}
+//       {loading && <Loader />}
+//       <ul className={styles.list}>
+//         {actors.map((actor) => (
+//           <li key={actor.id} className={styles.listItem}>
+//             <div className={styles.box}>
+//               <img
+//                 src={
+//                   actor.profile_path
+//                     ? `https://image.tmdb.org/t/p/w500${actor.profile_path}`
+//                     : defaultImg
+//                 }
+//                 alt="Actor photo"
+//                 width={100}
+//                 height={140}
+//               />
+//               <p className={styles.name}>{actor.name}</p>
+//               <p className={styles.hero}>Character:{actor.character}</p>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+// export default Cast;
